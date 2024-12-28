fa7c25d7b91e43983c476392c2cec83c4f70ca18
Jakub Zajega <jakub.zajega@gmail.com>
2024-12-28 17:55:58 +0100

    SmartToysStore - completed

---
+++ SmartToysStore.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SmartToysStore.DataAccess.Data;
+using SmartToysStore.Models;
+using SmartToysStore.Utility;
+
+namespace SmartToysStore.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+        
+        public DbInitializer(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+        public void Initialize()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@smarttoysstore.com",
+                    Email = "admin@smarttoysstore.com",
+                    Name = "Jan Kowalski",
+                    PhoneNumber = "123456789",
+                    StreetAddress = "test 123 Ave",
+                    State = "NY",
+                    PostalCode = "23422",
+                    City = "New York"
+
+                }, "Admin123#").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@smarttoysstore.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+
+            return;
+        }
+    }
+}
